49601348f810837ea0940148e32fcf5d79e45e25
Orel_Joel <ndediesso@gmail.com>
2026-06-12 02:48:57 +0100

    Suppression de fichiers

--- frontend/component2/MarketIntelligence.test.js
+++
@@ -1,9 +0,0 @@
-// components/MarketIntelligence.test.js
-import { render, screen } from '@testing-library/react';
-import GlobalMarketIntelligence from './GlobalMarketIntelligence';
-
-test('Vérifie si le composant de test existe', () => {
-  render(<GlobalMarketIntelligence />);
-  // Ce test échouera jusqu'à ce que ton composant soit lié à l'API
-  console.log("✅ Structure de test créée. Prêt à tester l'API.");
-});
